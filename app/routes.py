8b28115b5a1c6761347278c8ca6d669261af6e99
JuanELo_O <jed.mejia.lorenzo@gmail.com>
2020-05-11 16:44:10 -0500

    Removed test method

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,3 @@ root = Blueprint('root', __name__, url_prefix='/')
 def rush_hour(google_id):
     j_response = populartimes.get_id(os.environ['API_KEY'], google_id)
     return j_response
-
-@root.route("/", methods=['GET'])
-def helloWorld():
-    return "Hello, cross-origin-world!"
