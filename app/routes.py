b51fb48e42683a58bf8ea7e936ff2502e8345b8d
JuanELo_O <jed.mejia.lorenzo@gmail.com>
2020-05-11 16:03:33 -0500

    Adjust test service (temporal)

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,6 @@ def rush_hour(google_id):
     j_response = populartimes.get_id(os.environ['API_KEY'], google_id)
     return j_response
 
-@root.route("api/v1/", methods=['GET'])
+@root.route("/", methods=['GET'])
 def helloWorld():
     return "Hello, cross-origin-world!"
