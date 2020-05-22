6fd2b88b2f29e8b3574173e802dda0b6450bd8ef
JuanELo_O <jed.mejia.lorenzo@gmail.com>
2020-05-22 18:02:15 -0500

    Added queryparam to return only current occupancy

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,24 @@
 from flask import Blueprint
+from flask import request
+from flask import jsonify
 import populartimes
 import os
 
 root = Blueprint('root', __name__, url_prefix='/')
 
-@root.route('/api/v1/rush_hour/<google_id>', methods=['GET'])
+
+@root.route('/api/v1/rush_hour/<google_id>', methods=['GET'], )
 def rush_hour(google_id):
+    current = request.args.get('current')
     j_response = populartimes.get_id(os.environ['API_KEY'], google_id)
-    return j_response
+    if current == 'true':
+        if 'current_popularity' in j_response:
+            return jsonify(
+                occupancy=j_response['current_popularity']
+            )
+        else:
+            return jsonify(
+                occupancy=None
+            )
+    else:
+        return j_response
